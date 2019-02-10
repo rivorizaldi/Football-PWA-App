838c98cd0c22ad052afb52d7ef56c66cb8f86d23
Rivo Rizaldi <38255538+rivorizaldi@users.noreply.github.com>
2019-02-10 21:07:47 +0700

    Update push.js

--- With WorkBox/public/src/js/push.js
+++ With WorkBox/public/src/js/push.js
@@ -2,18 +2,18 @@ var webPush = require("web-push");
 
 var pushSubscription = {
   endpoint:
-    "https://android.googleapis.com/gcm/send/fmT2rizNAwo:APA91bFgveiEGI8RU8Ny7nDVPWhbReKvwApsihZ8HlA_uWAv5w4QO7kzl0tpsEPLyMB-O9qjRR72ZZR3H0Z2oLoqELueTso_G-mrIlS_ES_NBTSV2EGJVf49hYBifQqZeuTvEDRdwWBX",
+    "YOUR_ENDPOINT",
   keys: {
     p256dh:
-      "BHO2is4++/X0NegocV1CoAjMKB9IdbVYlCMCZ+aN4I46Jpk+BNSNSU8kXZuoIYe2Mh0MvzqIbVUFoIU/Her3leE=",
-    auth: "SI27K1nL6hdHiqKuHiYWPQ=="
+      "YOUR_P256DH",
+    auth: "YOUR_AUTH_KEY"
   }
 };
 
 var payload = "You Have Subscribed!";
 
 var options = {
-  gcmAPIKey: "AIzaSyBFBMI7c66cFXdS5Lf5MpiaY2YEG-3gHC8",
+  gcmAPIKey: "YOUR_LEGACY_KEY(FROM FIREBASE)",
   TTL: 60
 };
 
